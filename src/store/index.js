6ae003cf30788a56f6cda871ae2da2a005d64bef
David Saint <topstronics@gmail.com>
2018-05-26 19:53:09 +0100

    Changed the match to uefa finals

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ import { naijaSupporters } from './naija_supporters/store'
 export const store = new Vuex.Store({
 	state: {
 		token: 'Gfv90zzwtXqD533oy4okBIXIrhMnNwU6Szie0exeR5SfyiJwFehZXZxeIX2O',
-		season_id: 11759, // World-cup: 892
+		season_id: 7907, // World-cup: 892
 		time: 1450000,
     coke_token: '1104208046.a5a7f83.86773a3a3e9a4c119d65e274dacc76e5', // instagram token for naija supporters
     brides_token: '1104208046.a5a7f83.86773a3a3e9a4c119d65e274dacc76e5' // instagram token for bridal
