306607a3059d3ce7a9253895f6bf0b8b5e42c48e
David Saint <topstronics@gmail.com>
2018-05-29 16:45:21 +0100

    Second iteration to the commentary page

--- src/components/LiveCommentary.vue
+++ src/components/LiveCommentary.vue
@@ -4,7 +4,21 @@
       <img src="../assets/img/left-bottle.png" height="100%">
     </div>
     <div class="content">
-      
+      <div class="top">
+        <img src="../assets/img/vea.svg" width="100%">
+        <p>68:00</p>
+      </div>
+      <div class="center">
+        <div class="t">
+          Russia vs Saudi Arabia - Live
+        </div>
+        <div class="b">
+          
+        </div>
+      </div>
+      <div class="bottom">
+        <img src="../assets/img/xea.svg" width="100%">
+      </div>
     </div>
     <div class="right">
       <img src="../assets/img/right-bottle.png" height="100%">
