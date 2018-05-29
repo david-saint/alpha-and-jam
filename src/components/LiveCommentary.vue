cef75c23eed2b2a86afc82b0b4e6a4eb5d80c203
David Saint <topstronics@gmail.com>
2018-05-29 17:46:43 +0100

    completed the front-end of the commentary Page

--- src/components/LiveCommentary.vue
+++ src/components/LiveCommentary.vue
@@ -2,26 +2,39 @@
   <div class="container">
     <div class="left">
       <img src="../assets/img/left-bottle.png" height="100%">
+      <div class="image">
+        <img src="../assets/img/18702.png" height="100%">
+      </div>
     </div>
     <div class="content">
       <div class="top">
         <img src="../assets/img/vea.svg" width="100%">
-        <p>68:00</p>
+        <p>00:00</p>
       </div>
       <div class="center">
         <div class="t">
           Russia vs Saudi Arabia - Live
         </div>
         <div class="b">
-          
+          <div>
+            <p class="time">00"</p> <p>Nothing seems to be happening ^_^</p>
+          </div>
         </div>
       </div>
       <div class="bottom">
         <img src="../assets/img/xea.svg" width="100%">
+        <div class="score">
+          <img src="" class="circle">
+          <p>0 - 0</p>
+          <img src="" class="circle">
+        </div>
       </div>
     </div>
     <div class="right">
       <img src="../assets/img/right-bottle.png" height="100%">
+      <div class="image">
+        <img src="../assets/img/18702.png" height="100%">
+      </div>
     </div>
   </div>
 </template>
