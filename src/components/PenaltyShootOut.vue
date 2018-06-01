0f931038cdbc0a65e5b6f96b0e3db3e80b34ba55
David Saint <topstronics@gmail.com>
2018-06-01 01:47:41 +0100

    Finished the basics of penalty

---
+++ src/components/PenaltyShootOut.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="left">
+      <img src="../assets/img/left-bottle.png" height="100%">
+      <div class="image">
+        <img src="../assets/img/18702.png" height="100%">
+      </div>
+    </div>
+    <div class="content">
+      <div class="top">
+        <img src="../assets/img/russia.svg" alt="">
+      </div>
+      <p class="pk-t">Penalty shoot-out</p>
+
+      <div class="pk-teams">
+        <div class="pk-team">
+          <p>RUS</p>
+          <div>
+            <span class="goal"></span>
+            <span class="goal"></span>
+            <span class="miss"></span>
+            <span class="goal"></span>
+            <span></span>
+          </div>
+        </div>
+        <div class="pk-team">
+          <p>SDA</p>
+          <div>
+            <span class="goal"></span>
+            <span class="goal"></span>
+            <span class="goal"></span>
+            <span class="goal"></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+  
+      <div class="bottom">
+        <img src="../assets/img/sponsors.svg" alt="">
+      </div>
+    </div>
+    <div class="right">
+      <img src="../assets/img/right-bottle.png" height="100%">
+      <div class="image">
+        <img src="../assets/img/18562.png" height="100%">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+  @import '../sass/penalty_shootout.scss'
+</style>
