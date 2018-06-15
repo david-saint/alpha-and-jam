014f036cfbeb300319a39c36965712fae1a02a6e
David Saint <topstronics@gmail.com>
2018-06-15 18:26:19 +0100

    First iteration to changing the bottle

--- src/components/PenaltyShootOut.vue
+++ src/components/PenaltyShootOut.vue
@@ -2,9 +2,13 @@
   <div class="container">
     <div class="left">
       <img src="../assets/img/left-bottle.png" height="100%">
-      <div class="image">
-        <img src="../assets/img/18702.png" height="100%">
-      </div>
+      <svg class="svg" viewBox="0 0 183 118" fill="#FFFFFF">
+        <clipPath id="clip-path">
+          <path d="M505.665 2.5L180.165 0L4.66553 4.5C-1.35787 181.604 -1.20848 280.898 3.16546 458C111.964 463.027 173.332 464.513 283.665 464H402.665L500.165 460.5L588.665 456.5L591.665 299V135L587.165 5.5L505.665 2.5Z" transform="translate(-180.166 0.5)" fill="#C4C4C4"/>
+        </clipPath>
+        <!--<image clip-path="url(#clip-path)" :xlink:href="localTeam.flag"  :src="localTeam.flag" alt="Image" width="100%" class="svg__image" />-->
+        <image clip-path="url(#clip-path)" :xlink:href="require(`../assets/img/15251.png`)"  :src="require(`../assets/img/15251.png`)" alt="Image" width="100%" class="svg__image" />
+      </svg>
     </div>
     <div class="content">
       <div class="top">
@@ -34,7 +38,7 @@
           </div>
         </div>
       </div>
-  
+      
       <div class="bottom">
         <img src="../assets/img/sponsors.png" alt="">
       </div>
@@ -55,5 +59,5 @@
 </script>
 
 <style lang="scss">
-  @import '../sass/penalty_shootout.scss'
+@import '../sass/penalty_shootout.scss'
 </style>
