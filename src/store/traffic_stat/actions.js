da862dcac89618567a921d14199dd43dd482b1f1
David Saint <topstronics@gmail.com>
2018-12-18 03:11:10 +0100

    Modified to use actual data

--- src/store/traffic_stat/actions.js
+++ src/store/traffic_stat/actions.js
@@ -3,22 +3,29 @@ export default {
     function getTrafficData() {
       // build the url structure
       // https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin.long},${origin.lat}&destinations=${destination.long},${destination.lat}&key=${rootState.google}
-      const url = `https://remembrance-lumberjack-69271.herokuapp.com/?originLong=${origin.long}&originLat=${origin.lat}&destinationLong=${destination.long}&destinationLat=${destination.lat}`;
+      const url = `https://traffic-backend-node.herokuapp.com/`;
       // call the endpoint
-      axios.get(url)
+      axios.post(url, {
+        originLat: origin.lat,
+        originLong: origin.long,
+        destinationLat: destination.lat,
+        destinationLong: destination.long})
         .then(response => {
+          console.log(url);
           // defactor the response
           const data = response.data;
+          console.log(data);
           // get the origin address from the response
-          const origin = data.origin_addresses[0].split(',')[0];
+          const origin = `${data.origin_addresses[0].split(',')[0]} to ${data.destination_addresses[0].split(',')[0]}`;
           // commit the origin address to the store
           commit('setTrafficOrign', origin);
           // get the array containing important values
           const el = data.rows[0].elements[0];
           // calculate the speed from the distance and time
-          const speed = el.distance.value / el.duration.value;
+          const speed = el.distance.value / el.duration_in_traffic.value;
           // commit the speed to the stores
-          commit('setTrafficCar', el.duration.text);
+          commit('setTrafficCar', el.duration_in_traffic.text);
+          console.log(el.duration_in_traffic.text);
           // determine what color to commit based on speed
           if (speed >= 20) {
             // this is when there is no traffic
@@ -30,10 +37,6 @@ export default {
             // this is when ther's alot of traffic
             commit('setTrafficColor', '#D93600');
           }
-          // get the time for walkin
-          axios.get(`${url}&mode=walking`).then(response => { commit('setTrafficWalk', response.data.rows[0].elements[0].duration.text) });
-          // get the time for trains
-          axios.get(`${url}&mode=transit`).then(response => { commit('setTrafficTrain', response.data.rows[0].elements[0].duration.text) });
         })
         .catch(error => {
           console.error(error)
